28172ab76a7a94a7aa152ae231c8c36e4308341c
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2020-07-25 16:36:44 +0200

    Setup a simple JMS message sender and receiver via ActiveMQ

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -3989566832904063827L;
+
+    private UUID uuid;
+    private String message;
+}
